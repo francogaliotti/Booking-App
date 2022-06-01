889bee9057fbfec9cc149336a907fab7995c84e3
francogaliotti <francogaliotti@gmail.com>
2022-06-01 20:10:15 -0300

    http requests

--- server/src/routes/HotelRoutes.js
+++ server/src/routes/HotelRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {createHotel, updateHotel, deleteHotel, findOneHotel, findAllHotels} from '../controllers/HotelController.js'
+import { verifyAdmin } from '../utils/verifyToken.js'
 const router = express.Router()
 
 router.get('/', findAllHotels)
 router.get('/:id', findOneHotel)
-router.post("/", createHotel)
-router.put('/:id', updateHotel)
-router.delete('/:id', deleteHotel)
+router.post("/", verifyAdmin, createHotel)
+router.put('/:id', verifyAdmin, updateHotel)
+router.delete('/:id', verifyAdmin, deleteHotel)
 
 
 export default router
